81a101b1db5bd0e1d576a9b6e045510d762a364b
Jing Li <jing_li1998@hotmail.com>
2020-07-05 14:40:53 +1000

    add breakpoints object file

---
+++ src/utils/breakpoints.js
@@ -0,0 +1,15 @@
+const size = {
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
+  xl: "1440px",
+};
+
+const breakpoints = {
+  sm: `(min-width: ${size.sm})`,
+  md: `(min-width: ${size.md})`,
+  lg: `(min-width: ${size.lg})`,
+  xl: `(min-width: ${size.xl})`,
+};
+
+export default breakpoints;
